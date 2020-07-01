c265554a09165515c05d59e69ea36eed0458f865
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 02:16:07 +0700

    Update details.tsx

--- src/modules/orders/edit/components/details.tsx
+++ src/modules/orders/edit/components/details.tsx
@@ -1,72 +1,81 @@
-import Paper from "material-ui/Paper"
-import React from "react"
+import { Paper } from "@material-ui/core"
+import React, { useEffect } from "react"
 import OrderCustomer from "./customer"
 import OrderItems from "./items"
 import style from "./style.css"
 import OrderSummary from "./summary"
 import OrderTotals from "./totals"
 
-export default class OrderDetails extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const OrderDetails = (
+  props: Readonly<{
+    order
+    settings
+    onItemDelete
+    onItemUpdate
+    onShippingAddressUpdate
+    onOrderSummaryUpdate
+    onCheckout
+    processingCheckout
+    fetchData
+    clearData
+  }>
+) => {
+  const {
+    order,
+    settings,
+    onItemDelete,
+    onItemUpdate,
+    onShippingAddressUpdate,
+    onOrderSummaryUpdate,
+    onCheckout,
+    processingCheckout,
+    fetchData,
+    clearData,
+  } = props
 
-  componentDidMount() {
-    this.props.fetchData()
-  }
+  useEffect(() => {
+    fetchData()
+    return () => clearData()
+  }, [])
 
-  componentWillUnmount() {
-    this.props.clearData()
-  }
+  if (!order) return null
 
-  render() {
-    const {
-      order,
-      settings,
-      onItemDelete,
-      onItemUpdate,
-      onShippingAddressUpdate,
-      onOrderSummaryUpdate,
-      onCheckout,
-      processingCheckout,
-    } = this.props
-    if (!order) return null
-
-    return (
-      <div className="row row--no-gutter col-full-height">
-        <div className="col-xs-12 col-sm-5 col-md-4 col--no-gutter scroll col-full-height">
-          <OrderSummary
+  return (
+    <div className="row row--no-gutter col-full-height">
+      <div className="col-xs-12 col-sm-5 col-md-4 col--no-gutter scroll col-full-height">
+        <OrderSummary
+          order={order}
+          settings={settings}
+          onOrderSummaryUpdate={onOrderSummaryUpdate}
+          onCheckout={onCheckout}
+          processingCheckout={processingCheckout}
+        />
+        <OrderCustomer
+          order={order}
+          settings={settings}
+          onShippingAddressUpdate={onShippingAddressUpdate}
+        />
+      </div>
+      <div className="col-xs-12 col-sm-7 col-md-8 col--no-gutter scroll col-full-height">
+        <Paper className="paper-box" elevation={1}>
+          <OrderItems
             order={order}
             settings={settings}
-            onOrderSummaryUpdate={onOrderSummaryUpdate}
-            onCheckout={onCheckout}
-            processingCheckout={processingCheckout}
+            onItemDelete={onItemDelete}
+            onItemUpdate={onItemUpdate}
           />
-          <OrderCustomer
-            order={order}
-            settings={settings}
-            onShippingAddressUpdate={onShippingAddressUpdate}
-          />
-        </div>
-        <div className="col-xs-12 col-sm-7 col-md-8 col--no-gutter scroll col-full-height">
-          <Paper className="paper-box" zDepth={1}>
-            <OrderItems
-              order={order}
-              settings={settings}
-              onItemDelete={onItemDelete}
-              onItemUpdate={onItemUpdate}
-            />
-            <div className={style.innerBox}>
-              <div className="row">
-                <div className="col-xs-6" />
-                <div className="col-xs-6">
-                  <OrderTotals order={order} settings={settings} />
-                </div>
+          <div className={style.innerBox}>
+            <div className="row">
+              <div className="col-xs-6" />
+              <div className="col-xs-6">
+                <OrderTotals order={order} settings={settings} />
               </div>
             </div>
-          </Paper>
-        </div>
+          </div>
+        </Paper>
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default OrderDetails
