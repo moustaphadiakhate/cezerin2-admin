1f8aa9face4a7ace98f4c1524f3683e0282daeb4
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 20:33:06 +0700

    Update form.tsx

--- src/modules/settings/tokens/list/components/form.tsx
+++ src/modules/settings/tokens/list/components/form.tsx
@@ -1,8 +1,6 @@
-import Divider from "material-ui/Divider"
-import FontIcon from "material-ui/FontIcon"
-import { List, ListItem } from "material-ui/List"
-import Paper from "material-ui/Paper"
-import React from "react"
+import { Divider, List, ListItem, Paper } from "@material-ui/core"
+import { KeyboardArrowRight } from "@material-ui/icons"
+import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
 import messages from "../../../../../lib/text"
 
@@ -13,45 +11,43 @@ const TokenItem = ({ token }) => (
       to={`/settings/tokens/${token.id}`}
       style={{ textDecoration: "none" }}
     >
-      <ListItem
-        rightIcon={
-          <FontIcon className="material-icons">keyboard_arrow_right</FontIcon>
-        }
-        primaryText={
-          <div className="row">
-            <div className="col-xs-6">{token.name}</div>
-            <div className="col-xs-6" style={{ color: "rgba(0, 0, 0, 0.4)" }}>
-              {token.email}
-            </div>
+      <ListItem>
+        {" "}
+        <div className="row">
+          <div className="col-xs-6">{token.name}</div>
+          <div className="col-xs-6" style={{ color: "rgba(0, 0, 0, 0.4)" }}>
+            {token.email}
           </div>
-        }
-      />
+        </div>{" "}
+        <KeyboardArrowRight className="material-icons" />
+      </ListItem>
     </Link>
   </>
 )
 
-export default class TokensList extends React.Component {
-  componentDidMount() {
-    this.props.onLoad()
-  }
+const TokensList = (props: Readonly<{ tokens; onLoad }>) => {
+  const { tokens, onLoad } = props
 
-  render() {
-    const { tokens } = this.props
-    const listItems = tokens.map((token, index) => (
-      <TokenItem key={index} token={token} />
-    ))
+  useEffect(() => {
+    onLoad()
+  }, [])
 
-    return (
-      <>
-        <div style={{ margin: 20, color: "rgba(0, 0, 0, 0.52)" }}>
-          {messages.settings_tokenHelp}
+  const listItems = tokens.map((token, index) => (
+    <TokenItem key={index} token={token} />
+  ))
+
+  return (
+    <>
+      <div style={{ margin: 20, color: "rgba(0, 0, 0, 0.52)" }}>
+        {messages.settings_tokenHelp}
+      </div>
+      <Paper className="paper-box" elevation={1}>
+        <div style={{ width: "100%" }}>
+          <List style={{ padding: 0 }}>{listItems}</List>
         </div>
-        <Paper className="paper-box" zDepth={1}>
-          <div style={{ width: "100%" }}>
-            <List style={{ padding: 0 }}>{listItems}</List>
-          </div>
-        </Paper>
-      </>
-    )
-  }
+      </Paper>
+    </>
+  )
 }
+
+export default TokensList
