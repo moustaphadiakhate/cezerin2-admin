c5604855cecd330279bb61b8e9060ead8f79a6f9
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 20:27:03 +0700

    Update form.tsx

--- src/modules/settings/payments/components/form.tsx
+++ src/modules/settings/payments/components/form.tsx
@@ -1,56 +1,46 @@
-import Divider from "material-ui/Divider"
-import FontIcon from "material-ui/FontIcon"
-import { List, ListItem } from "material-ui/List"
-import Paper from "material-ui/Paper"
-import React from "react"
+import { Divider, List, ListItem, Paper } from "@material-ui/core"
+import { KeyboardArrowRight } from "@material-ui/icons"
+import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
 
-const MethodItem = ({ method }) => (
+const MethodItem = ({ method }: { method }) => (
   <>
     <Divider />
     <Link
       to={`/settings/payments/${method.id}`}
       style={{ textDecoration: "none" }}
     >
-      <ListItem
-        rightIcon={
-          <FontIcon className="material-icons">keyboard_arrow_right</FontIcon>
-        }
-        style={!method.enabled ? { color: "rgba(0, 0, 0, 0.3)" } : {}}
-        primaryText={
-          <div className="row">
-            <div className="col-xs-6">{method.name}</div>
-            <div className="col-xs-6" style={{ color: "rgba(0, 0, 0, 0.4)" }}>
-              {method.description}
-            </div>
+      <ListItem style={!method.enabled ? { color: "rgba(0, 0, 0, 0.3)" } : {}}>
+        <KeyboardArrowRight className="material-icons" />{" "}
+        <div className="row">
+          <div className="col-xs-6">{method.name}</div>
+          <div className="col-xs-6" style={{ color: "rgba(0, 0, 0, 0.4)" }}>
+            {method.description}
           </div>
-        }
-      />
+        </div>
+      </ListItem>
     </Link>
   </>
 )
 
-export default class EmailSettings extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const EmailSettings = (props: Readonly<{ paymentMethods; onLoad }>) => {
+  const { paymentMethods, onLoad } = props
 
-  componentDidMount() {
-    this.props.onLoad()
-  }
+  useEffect(() => {
+    onLoad()
+  }, [])
 
-  render() {
-    const { paymentMethods } = this.props
-    const methods = paymentMethods.map((method, index) => (
-      <MethodItem key={index} method={method} />
-    ))
+  const methods = paymentMethods.map((method, index) => (
+    <MethodItem key={index} method={method} />
+  ))
 
-    return (
-      <Paper className="paper-box" zDepth={1}>
-        <div style={{ width: "100%" }}>
-          <List style={{ padding: 0 }}>{methods}</List>
-        </div>
-      </Paper>
-    )
-  }
+  return (
+    <Paper className="paper-box" elevation={1}>
+      <div style={{ width: "100%" }}>
+        <List style={{ padding: 0 }}>{methods}</List>
+      </div>
+    </Paper>
+  )
 }
+
+export default EmailSettings
