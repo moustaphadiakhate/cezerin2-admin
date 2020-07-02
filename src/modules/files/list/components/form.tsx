e0e3aa0729a596a55f0f15843ffc5f15f03d7a4e
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-03 05:02:37 +0700

    Update form.tsx

--- src/modules/files/list/components/form.tsx
+++ src/modules/files/list/components/form.tsx
@@ -1,10 +1,8 @@
-import FontIcon from "material-ui/FontIcon"
-import IconButton from "material-ui/IconButton"
+import { IconButton, MenuItem, Paper } from "@material-ui/core"
+import { MoreVert } from "@material-ui/icons"
 import IconMenu from "material-ui/IconMenu"
-import MenuItem from "material-ui/MenuItem"
-import Paper from "material-ui/Paper"
 import moment from "moment"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import * as helper from "../../../../lib/helper"
 import messages from "../../../../lib/text"
 import DeleteConfirmation from "../../../../modules/shared/deleteConfirmation"
@@ -13,102 +11,80 @@ import style from "./style.css"
 
 const iconButtonElement = (
   <IconButton touch>
-    <FontIcon color="rgb(189, 189, 189)" className="material-icons">
-      more_vert
-    </FontIcon>
+    <MoreVert color="secondary" className="material-icons" />
   </IconButton>
 )
 
-class FileItem extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      openDelete: false,
-    }
-  }
+const FileItem = props => {
+  const [openDelete, setopenDelete] = useState(false)
 
-  showDelete = () => {
-    this.setState({ openDelete: true })
-  }
+  const { file, settings, onDelete } = props
 
-  hideDelete = () => {
-    this.setState({ openDelete: false })
-  }
-
-  handleDelete = () => {
-    const fileName = this.props.file.file
-    this.props.onDelete(fileName)
-    this.hideDelete()
-  }
-
-  render() {
-    const { file, settings } = this.props
+  const handleDelete = () => {
     const fileName = file.file
-    const fileUrl = `${settings.assetServerDomain}/${file.file}`
-    const modifiedDate = moment(file.modified)
-    const modifiedDateFormated = modifiedDate.format(`${settings.date_format}`)
-    const fileSizeFormated = helper.formatFileSize(file.size)
-
-    return (
-      <div className={`${style.item} row row--no-gutter middle-xs`}>
-        <div className={`${style.name} col-xs-5`}>
-          <a href={fileUrl} target="_blank" rel="noopener">
-            {file.file}
-          </a>
-        </div>
-        <div className={`${style.date} col-xs-3`}>{modifiedDateFormated}</div>
-        <div className={`${style.size} col-xs-2`}>{fileSizeFormated}</div>
-        <div className={`${style.more} col-xs-2`}>
-          <IconMenu iconButtonElement={iconButtonElement}>
-            <MenuItem onClick={this.showDelete}>
-              {messages.actions_delete}
-            </MenuItem>
-          </IconMenu>
-          <DeleteConfirmation
-            open={this.state.openDelete}
-            isSingle
-            itemsCount={1}
-            itemName={fileName}
-            onCancel={this.hideDelete}
-            onDelete={this.handleDelete}
-          />
-        </div>
-      </div>
-    )
+    onDelete(fileName)
+    setopenDelete(false)
   }
+
+  const fileName = file.file
+  const fileUrl = `${settings.assetServerDomain}/${file.file}`
+  const modifiedDate = moment(file.modified)
+  const modifiedDateFormated = modifiedDate.format(`${settings.date_format}`)
+  const fileSizeFormated = helper.formatFileSize(file.size)
+
+  return (
+    <div className={`${style.item} row row--no-gutter middle-xs`}>
+      <div className={`${style.name} col-xs-5`}>
+        <a href={fileUrl} target="_blank" rel="noopener">
+          {file.file}
+        </a>
+      </div>
+      <div className={`${style.date} col-xs-3`}>{modifiedDateFormated}</div>
+      <div className={`${style.size} col-xs-2`}>{fileSizeFormated}</div>
+      <div className={`${style.more} col-xs-2`}>
+        <IconMenu iconButtonElement={iconButtonElement}>
+          <MenuItem onClick={() => setopenDelete(true)}>
+            {messages.actions_delete}
+          </MenuItem>
+        </IconMenu>
+        <DeleteConfirmation
+          open={openDelete}
+          isSingle
+          itemsCount={1}
+          itemName={fileName}
+          onCancel={() => setopenDelete(false)}
+          onDelete={handleDelete}
+        />
+      </div>
+    </div>
+  )
 }
 
-class FileList extends React.Component {
-  componentDidMount() {
-    this.props.onLoad()
-  }
+const FileList = props => {
+  const { files, settings, onDelete, onUpload, uploading, onLoad } = props
 
-  render() {
-    const { files, settings, onDelete, onUpload, uploading } = this.props
-    const listItems = files.map((file, index) => (
-      <FileItem
-        key={index}
-        file={file}
-        settings={settings}
-        onDelete={onDelete}
-      />
-    ))
+  useEffect(() => {
+    onLoad()
+  }, [])
 
-    return (
-      <>
-        <div className={`${style.head} row row--no-gutter`}>
-          <div className="col-xs-5">{messages.fileName}</div>
-          <div className="col-xs-3">{messages.fileModified}</div>
-          <div className="col-xs-2">{messages.fileSize}</div>
-          <div className="col-xs-2" />
-        </div>
-        <Paper className="paper-box" zDepth={1}>
-          {listItems}
-        </Paper>
-        <FileUploader onUpload={onUpload} uploading={uploading} />
-      </>
-    )
-  }
+  const listItems = files.map((file, index) => (
+    <FileItem key={index} file={file} settings={settings} onDelete={onDelete} />
+  ))
+
+  return (
+    <>
+      <div className={`${style.head} row row--no-gutter`}>
+        <div className="col-xs-5">{messages.fileName}</div>
+        <div className="col-xs-3">{messages.fileModified}</div>
+        <div className="col-xs-2">{messages.fileSize}</div>
+        <div className="col-xs-2" />
+      </div>
+      <Paper className="paper-box" zDepth={1}>
+        {listItems}
+      </Paper>
+      <FileUploader onUpload={onUpload} uploading={uploading} />
+    </>
+  )
 }
 
 export default FileList
