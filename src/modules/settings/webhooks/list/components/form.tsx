f80c358410f517a748a7beddb73099d1cdef247e
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 20:50:21 +0700

    Update form.tsx

--- src/modules/settings/webhooks/list/components/form.tsx
+++ src/modules/settings/webhooks/list/components/form.tsx
@@ -1,8 +1,6 @@
-import Divider from "material-ui/Divider"
-import FontIcon from "material-ui/FontIcon"
-import { List, ListItem } from "material-ui/List"
-import Paper from "material-ui/Paper"
-import React from "react"
+import { Divider, List, ListItem, Paper } from "@material-ui/core"
+import { KeyboardArrowRight } from "@material-ui/icons"
+import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
 import messages from "../../../../../lib/text"
 
@@ -19,46 +17,45 @@ const WebhookItem = ({ webhook }) => {
         style={{ textDecoration: "none" }}
       >
         <ListItem
-          rightIcon={
-            <FontIcon className="material-icons">keyboard_arrow_right</FontIcon>
-          }
           style={!webhook.enabled ? { color: "rgba(0, 0, 0, 0.3)" } : {}}
-          primaryText={
-            <div className="row">
-              <div className="col-xs-6">{webhook.url}</div>
-              <div className="col-xs-6" style={{ color: "rgba(0, 0, 0, 0.4)" }}>
-                {events}
-              </div>
+        >
+          {" "}
+          <div className="row">
+            <div className="col-xs-6">{webhook.url}</div>
+            <div className="col-xs-6" style={{ color: "rgba(0, 0, 0, 0.4)" }}>
+              {events}
             </div>
-          }
-        />
+          </div>
+          <KeyboardArrowRight className="material-icons" />
+        </ListItem>
       </Link>
     </>
   )
 }
 
-export default class WebhooksList extends React.Component {
-  componentDidMount() {
-    this.props.onLoad()
-  }
+const WebhooksList = (props: Readonly<{ webhooks; onLoad }>) => {
+  const { webhooks, onLoad } = props
 
-  render() {
-    const { webhooks } = this.props
-    const listItems = webhooks.map((webhook, index) => (
-      <WebhookItem key={index} webhook={webhook} />
-    ))
+  useEffect(() => {
+    onLoad()
+  }, [])
 
-    return (
-      <>
-        <div style={{ margin: 20, color: "rgba(0, 0, 0, 0.52)" }}>
-          {messages.webhooksAbout}
+  const listItems = webhooks.map((webhook, index) => (
+    <WebhookItem key={index} webhook={webhook} />
+  ))
+
+  return (
+    <>
+      <div style={{ margin: 20, color: "rgba(0, 0, 0, 0.52)" }}>
+        {messages.webhooksAbout}
+      </div>
+      <Paper className="paper-box" elevation={1}>
+        <div style={{ width: "100%" }}>
+          <List style={{ padding: 0 }}>{listItems}</List>
         </div>
-        <Paper className="paper-box" zDepth={1}>
-          <div style={{ width: "100%" }}>
-            <List style={{ padding: 0 }}>{listItems}</List>
-          </div>
-        </Paper>
-      </>
-    )
-  }
+      </Paper>
+    </>
+  )
 }
+
+export default WebhooksList
