aa1a9f62b65098b26683c3a54cae0de306897202
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-03 04:21:06 +0700

    Update headButtons.tsx

--- src/modules/settings/webhooks/edit/components/headButtons.tsx
+++ src/modules/settings/webhooks/edit/components/headButtons.tsx
@@ -1,59 +1,44 @@
-import FontIcon from "material-ui/FontIcon"
-import IconButton from "material-ui/IconButton"
-import React from "react"
+import { IconButton } from "@material-ui/core"
+import { Delete } from "@material-ui/icons"
+import React, { useState } from "react"
 import messages from "../../../../../lib/text"
 import DeleteConfirmation from "../../../../../modules/shared/deleteConfirmation"
 
-export default class Buttons extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      openDelete: false,
-    }
+const Buttons = (props: Readonly<{ webhook; onDelete }>) => {
+  const { webhook, onDelete } = props
+
+  const [openDelete, setOpenDelete] = useState(false)
+  const deletePage = () => {
+    setOpenDelete(false)
+    onDelete(webhook.id)
   }
 
-  openDelete = () => {
-    this.setState({ openDelete: true })
-  }
+  const webhookName =
+    webhook && webhook.url && webhook.url.length > 0 ? webhook.url : "Draft"
 
-  closeDelete = () => {
-    this.setState({ openDelete: false })
+  if (webhook) {
+    return (
+      <>
+        <IconButton
+          touch
+          tooltipPosition="bottom-left"
+          tooltip={messages.actions_delete}
+          onClick={() => setOpenDelete(true)}
+        >
+          <Delete color="primary" className="material-icons" />
+        </IconButton>
+        <DeleteConfirmation
+          open={openDelete}
+          isSingle
+          itemsCount={1}
+          itemName={webhookName}
+          onCancel={() => setOpenDelete(false)}
+          onDelete={deletePage}
+        />
+      </>
+    )
   }
-
-  deletePage = () => {
-    this.setState({ openDelete: false })
-    this.props.onDelete(this.props.webhook.id)
-  }
-
-  render() {
-    const { webhook } = this.props
-    const webhookName =
-      webhook && webhook.url && webhook.url.length > 0 ? webhook.url : "Draft"
-
-    if (webhook) {
-      return (
-        <>
-          <IconButton
-            touch
-            tooltipPosition="bottom-left"
-            tooltip={messages.actions_delete}
-            onClick={this.openDelete}
-          >
-            <FontIcon color="#fff" className="material-icons">
-              delete
-            </FontIcon>
-          </IconButton>
-          <DeleteConfirmation
-            open={this.state.openDelete}
-            isSingle
-            itemsCount={1}
-            itemName={webhookName}
-            onCancel={this.closeDelete}
-            onDelete={this.deletePage}
-          />
-        </>
-      )
-    }
-    return null
-  }
+  return null
 }
+
+export default Buttons
