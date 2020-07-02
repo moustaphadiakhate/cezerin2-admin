00b76556a3b472ee38735a77263675cced1241cd
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 19:59:29 +0700

    Update index.tsx

--- src/modules/settings/emailTemplates/index.tsx
+++ src/modules/settings/emailTemplates/index.tsx
@@ -2,16 +2,16 @@ import { connect } from "react-redux"
 import { fetchEmailTemplate, updateEmailTemplate } from "../actions"
 import Form from "./components/form"
 
-const mapStateToProps = (state, ownProps) => ({
+const mapStateToProps = (state: { settings: { emailTemplate: string } }) => ({
   initialValues: state.settings.emailTemplate,
 })
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
+const mapDispatchToProps = (dispatch: Function, ownProps: any) => ({
   onLoad: () => {
     const { templateName } = ownProps.match.params
     dispatch(fetchEmailTemplate(templateName))
   },
-  onSubmit: values => {
+  onSubmit: (values: string) => {
     dispatch(updateEmailTemplate(values))
   },
 })
