2df10655824f288ee05deedf1a4f608f4169e21d
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 19:16:43 +0700

    Update index.tsx

--- src/modules/shared/imageUpload/index.tsx
+++ src/modules/shared/imageUpload/index.tsx
@@ -1,120 +1,111 @@
+import { Paper } from "@material-ui/core"
 import FontIcon from "material-ui/FontIcon"
 import IconButton from "material-ui/IconButton"
-import Paper from "material-ui/Paper"
 import Snackbar from "material-ui/Snackbar"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import Dropzone from "react-dropzone"
 import messages from "../../../lib/text"
 import style from "./style.css"
 
-class ImageUpload extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      imagePreview: this.props.imageUrl,
-    }
+const ImageUpload = (
+  props: Readonly<{ uploading; onDelete; imageUrl; onUpload; children }>
+) => {
+  const [imagePreview, setImagePreview] = useState(props.imageUrl)
+
+  const { uploading, imageUrl, onUpload, children } = props
+
+  const onDelete = () => {
+    setImagePreview(null)
+    props.onDelete()
   }
 
-  onDelete = () => {
-    this.setState({
-      imagePreview: null,
-    })
-    this.props.onDelete()
-  }
+  useEffect(() => {
+    setImagePreview(imageUrl)
+  }, [imageUrl])
 
-  componentWillReceiveProps(nextProps) {
-    this.setState({
-      imagePreview: nextProps.imageUrl,
-    })
-  }
-
-  onDrop = files => {
+  const onDrop = files => {
     const form = new FormData()
     form.append("file", files[0])
-    this.props.onUpload(form)
+    onUpload(form)
   }
 
-  render() {
-    const { imagePreview } = this.state
-    const { uploading } = this.props
+  const hasPreview = imagePreview !== null && imagePreview !== ""
+  const previewIsFileUrl = hasPreview ? imagePreview.startsWith("http") : null
 
-    const hasPreview = imagePreview !== null && imagePreview !== ""
-    const previewIsFileUrl = hasPreview ? imagePreview.startsWith("http") : null
+  let htmlPreview = (
+    <div className={style.noImage}>
+      <FontIcon
+        style={{ fontSize: 90, color: "#cccccc" }}
+        className="material-icons"
+      >
+        photo_camera
+      </FontIcon>
+      <div className={style.dropText}>{messages.help_dropHere}</div>
+    </div>
+  )
 
-    let htmlPreview = (
-      <div className={style.noImage}>
-        <FontIcon
-          style={{ fontSize: 90, color: "#cccccc" }}
-          className="material-icons"
-        >
-          photo_camera
-        </FontIcon>
-        <div className={style.dropText}>{messages.help_dropHere}</div>
-      </div>
-    )
+  if (hasPreview && previewIsFileUrl) {
+    htmlPreview = <img src={imagePreview} />
+  } else if (hasPreview && !previewIsFileUrl) {
+    htmlPreview = <img src={imagePreview} />
+  }
 
-    if (hasPreview && previewIsFileUrl) {
-      htmlPreview = <img src={imagePreview} />
-    } else if (hasPreview && !previewIsFileUrl) {
-      htmlPreview = <img src={imagePreview} />
-    }
+  return (
+    <Paper elevation={1} rounded={false} style={{ width: 200 }}>
+      <Dropzone
+        onDrop={onDrop}
+        multiple={false}
+        disableClick={hasPreview}
+        accept="image/*"
+        ref={node => {
+          dropzone = node
+        }}
+        style={{}}
+        className={style.dropzone}
+        activeClassName={style.dropzoneActive}
+        rejectClassName={style.dropzoneReject}
+      >
+        {({ getRootProps, getInputProps }) =>
+          children != null ? (
+            children
+          ) : (
+            <div {...getRootProps()}>
+              <input {...getInputProps()} />
+              <div className={style.preview}>{htmlPreview}</div>
+            </div>
+          )
+        }
+      </Dropzone>
 
-    return (
-      <Paper zDepth={1} rounded={false} style={{ width: 200 }}>
-        <Dropzone
-          onDrop={this.onDrop}
-          multiple={false}
-          disableClick={hasPreview}
-          accept="image/*"
-          ref={node => {
-            this.dropzone = node
+      <div className={style.footer}>
+        <IconButton
+          touch
+          tooltip={messages.actions_upload}
+          onClick={() => {
+            dropzone.open()
           }}
-          style={{}}
-          className={style.dropzone}
-          activeClassName={style.dropzoneActive}
-          rejectClassName={style.dropzoneReject}
+          tooltipPosition="top-right"
         >
-          {({ getRootProps, getInputProps }) =>
-            this.props.children != null ? (
-              this.props.children
-            ) : (
-              <div {...getRootProps()}>
-                <input {...getInputProps()} />
-                <div className={style.preview}>{htmlPreview}</div>
-              </div>
-            )
-          }
-        </Dropzone>
-
-        <div className={style.footer}>
+          <FontIcon color="rgba(0,0,0,0.5)" className="material-icons">
+            file_upload
+          </FontIcon>
+        </IconButton>
+        {hasPreview && (
           <IconButton
             touch
-            tooltip={messages.actions_upload}
-            onClick={() => {
-              this.dropzone.open()
-            }}
+            tooltip={messages.actions_delete}
+            onClick={onDelete}
             tooltipPosition="top-right"
           >
             <FontIcon color="rgba(0,0,0,0.5)" className="material-icons">
-              file_upload
+              delete
             </FontIcon>
           </IconButton>
-          {hasPreview && (
-            <IconButton
-              touch
-              tooltip={messages.actions_delete}
-              onClick={this.onDelete}
-              tooltipPosition="top-right"
-            >
-              <FontIcon color="rgba(0,0,0,0.5)" className="material-icons">
-                delete
-              </FontIcon>
-            </IconButton>
-          )}
-        </div>
-        <Snackbar open={uploading} message={messages.messages_uploading} />
-      </Paper>
-    )
-  }
+        )}
+      </div>
+      <Snackbar open={uploading} message={messages.messages_uploading} />
+    </Paper>
+  )
 }
+
 export default ImageUpload
