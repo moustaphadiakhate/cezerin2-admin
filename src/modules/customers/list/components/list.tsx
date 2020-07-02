f465bf5d1e856f3e7745effceb7c78dd57fd9b5f
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-03 04:43:25 +0700

    Update list.tsx

--- src/modules/customers/list/components/list.tsx
+++ src/modules/customers/list/components/list.tsx
@@ -2,56 +2,57 @@ import Divider from "material-ui/Divider"
 import FontIcon from "material-ui/FontIcon"
 import { List } from "material-ui/List"
 import RaisedButton from "material-ui/RaisedButton"
-import React from "react"
+import React, { useEffect } from "react"
 import messages from "../../../../lib/text"
 import Head from "./head"
 import CustomersListItem from "./item"
 import style from "./style.css"
 
-export default class CustomersList extends React.Component {
-  componentDidMount() {
-    this.props.onLoad()
-  }
+const CustomersList = props => {
+  const {
+    items,
+    selected,
+    loadingItems,
+    hasMore,
+    onSelect,
+    onSelectAll,
+    loadMore,
+    settings,
+  } = props
 
-  render() {
-    const {
-      items,
-      selected,
-      loadingItems,
-      hasMore,
-      onSelect,
-      onSelectAll,
-      loadMore,
-      settings,
-    } = this.props
-    const rows = items.map((item, index) => (
-      <CustomersListItem
-        key={index}
-        customer={item}
-        selected={selected}
-        onSelect={onSelect}
-        settings={settings}
-      />
-    ))
+  useEffect(() => {
+    props.onLoad()
+  }, [])
 
-    return (
-      <>
-        <List>
-          <Head onSelectAll={onSelectAll} />
-          <Divider />
-          {rows}
-          <div className={style.more}>
-            <RaisedButton
-              disabled={loadingItems || !hasMore}
-              label={messages.actions_loadMore}
-              labelPosition="before"
-              primary={false}
-              icon={<FontIcon className="material-icons">refresh</FontIcon>}
-              onClick={loadMore}
-            />
-          </div>
-        </List>
-      </>
-    )
-  }
+  const rows = items.map((item, index) => (
+    <CustomersListItem
+      key={index}
+      customer={item}
+      selected={selected}
+      onSelect={onSelect}
+      settings={settings}
+    />
+  ))
+
+  return (
+    <>
+      <List>
+        <Head onSelectAll={onSelectAll} />
+        <Divider />
+        {rows}
+        <div className={style.more}>
+          <RaisedButton
+            disabled={loadingItems || !hasMore}
+            label={messages.actions_loadMore}
+            labelPosition="before"
+            primary={false}
+            icon={<FontIcon className="material-icons">refresh</FontIcon>}
+            onClick={loadMore}
+          />
+        </div>
+      </List>
+    </>
+  )
 }
+
+export default CustomersList
