c1467ea384f3a0fe306dff075c8c9a537b82ec73
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 20:21:17 +0700

    Update selectShipping.tsx

--- src/modules/settings/paymentsEdit/components/selectShipping.tsx
+++ src/modules/settings/paymentsEdit/components/selectShipping.tsx
@@ -2,49 +2,49 @@ import Checkbox from "material-ui/Checkbox"
 import { List, ListItem } from "material-ui/List"
 import React, { useEffect, useState } from "react"
 
-const SelectShippingMethodsField =(props:Readonly<>) =>{\
-    const ids = Array.isArray(props.input.value) ? props.input.value : []
-     const [selectedIds,setSelectedIds] =useState(ids)
+const SelectShippingMethodsField = (
+  props: Readonly<{ input; shippingMethods }>
+) => {
+  const ids = Array.isArray(props.input.value) ? props.input.value : []
+  const [selectedIds, setSelectedIds] = useState(ids)
 
-  useEffect(() =>{
-    const newIds = Array.isArray(props.input.value)
-      ? props.input.value
-      : []
+  useEffect(() => {
+    const newIds = Array.isArray(props.input.value) ? props.input.value : []
     if (newIds !== selectedIds) {
       setSelectedIds(newIds)
     }
-  },[props.input])
+  }, [props.input])
 
-  const  onCheckboxChecked = methodId => {
+  const onCheckboxChecked = methodId => {
     let ids = selectedIds
     if (ids.includes(methodId)) {
       ids = ids.filter(id => id !== methodId)
     } else {
       ids.push(methodId)
     }
-  setSelectedIds(ids)
+    setSelectedIds(ids)
     props.input.onChange(ids)
   }
 
   const isCheckboxChecked = methodId => selectedIds.includes(methodId)
 
-    const items = props.shippingMethods.map(method => (
-      <ListItem
-        key={method.id}
-        leftCheckbox={
-          <Checkbox
-            checked={isCheckboxChecked(method.id)}
-            onCheck={() => {
-              onCheckboxChecked(method.id)
-            }}
-          />
-        }
-        primaryText={method.name}
-        secondaryText={method.description}
-      />
-    ))
+  const items = props.shippingMethods.map(method => (
+    <ListItem
+      key={method.id}
+      leftCheckbox={
+        <Checkbox
+          checked={isCheckboxChecked(method.id)}
+          onCheck={() => {
+            onCheckboxChecked(method.id)
+          }}
+        />
+      }
+      primaryText={method.name}
+      secondaryText={method.description}
+    />
+  ))
 
-    return <List>{items}</List>
-  }
+  return <List>{items}</List>
+}
 
-  export default SelectShippingMethodsField
+export default SelectShippingMethodsField
