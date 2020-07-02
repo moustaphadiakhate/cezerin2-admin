6d7d762619662bd5df694e6771eda7c27c6891c0
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 19:58:41 +0700

    Update form.tsx

--- src/modules/settings/emailTemplates/components/form.tsx
+++ src/modules/settings/emailTemplates/components/form.tsx
@@ -1,58 +1,58 @@
-import Paper from "material-ui/Paper"
-import RaisedButton from "material-ui/RaisedButton"
-import React from "react"
+import { Button, Paper } from "@material-ui/core"
+import React, { useEffect } from "react"
 import { Field, reduxForm } from "redux-form"
 import { TextField } from "redux-form-material-ui"
 import messages from "../../../../lib/text"
 import style from "./style.css"
 
-class EmailTemplate extends React.Component {
-  componentDidMount() {
-    this.props.onLoad()
-  }
+const EmailTemplate = (
+  props: Readonly<{ handleSubmit; pristine; submitting; onLoad }>
+) => {
+  const { handleSubmit, pristine, submitting, onLoad } = props
 
-  render() {
-    const { handleSubmit, pristine, submitting, initialValues } = this.props
+  useEffect(() => {
+    onLoad()
+  }, [])
 
-    return (
-      <form
-        onSubmit={handleSubmit}
-        style={{
-          display: "initial",
-          width: "100%",
-        }}
-      >
-        <Paper className="paper-box" zDepth={1}>
-          <div className={style.innerBox}>
-            <>
-              <Field
-                component={TextField}
-                fullWidth
-                name="subject"
-                floatingLabelText={messages.settings_emailSubject}
-              />
-              <Field
-                component={TextField}
-                fullWidth
-                name="body"
-                multiLine
-                floatingLabelText={messages.settings_emailBody}
-              />
-            </>
-          </div>
-          <div className="buttons-box">
-            <RaisedButton
-              type="submit"
-              label={messages.save}
-              primary
-              className={style.button}
-              disabled={pristine || submitting}
+  return (
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        display: "initial",
+        width: "100%",
+      }}
+    >
+      <Paper className="paper-box" elevation={1}>
+        <div className={style.innerBox}>
+          <>
+            <Field
+              component={TextField}
+              fullWidth
+              name="subject"
+              floatingLabelText={messages.settings_emailSubject}
             />
-          </div>
-        </Paper>
-      </form>
-    )
-  }
+            <Field
+              component={TextField}
+              fullWidth
+              name="body"
+              multiLine
+              floatingLabelText={messages.settings_emailBody}
+            />
+          </>
+        </div>
+        <div className="buttons-box">
+          <Button
+            type="submit"
+            color="primary"
+            className={style.button}
+            disabled={pristine || submitting}
+          >
+            {messages.save}
+          </Button>
+        </div>
+      </Paper>
+    </form>
+  )
 }
 
 export default reduxForm({ form: "EmailTemplate", enableReinitialize: true })(
