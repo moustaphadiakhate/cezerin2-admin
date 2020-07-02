05ccf2d780fa9154ba2614cb979eb3b70e5b6a30
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 19:49:14 +0700

    Update form.tsx

--- src/modules/settings/import/components/form.tsx
+++ src/modules/settings/import/components/form.tsx
@@ -1,50 +1,39 @@
-import FontIcon from "material-ui/FontIcon"
-import { List, ListItem } from "material-ui/List"
-import Paper from "material-ui/Paper"
-import React from "react"
+import { List, ListItem, Paper } from "@material-ui/core"
+import { KeyboardArrowRight } from "@material-ui/icons"
+import React, { useEffect } from "react"
 import { Link } from "react-router-dom"
 import messages from "../../../../lib/text"
 
-export default class ImportSettings extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const ImportSettings = (props: Readonly<{ onLoad }>) => {
+  const { onLoad } = props
 
-  componentDidMount() {
-    this.props.onLoad()
-  }
+  useEffect(() => {
+    onLoad()
+  }, [])
 
-  render() {
-    const { importSettings } = this.props
-
-    return (
-      <>
-        <Paper className="paper-box" zDepth={1}>
-          <div style={{ width: "100%" }}>
-            <List style={{ padding: 0 }}>
-              <Link
-                to="/settings/import/googlespreadsheet"
-                style={{ textDecoration: "none" }}
-              >
-                <ListItem
-                  rightIcon={
-                    <FontIcon className="material-icons">
-                      keyboard_arrow_right
-                    </FontIcon>
-                  }
-                  primaryText={
-                    <div className="row">
-                      <div className="col-xs-6">
-                        {messages.settings_spreadsheet}
-                      </div>
-                    </div>
-                  }
-                />
-              </Link>
-            </List>
-          </div>
-        </Paper>
-      </>
-    )
-  }
+  return (
+    <>
+      <Paper className="paper-box" elevation={1}>
+        <div style={{ width: "100%" }}>
+          <List style={{ padding: 0 }}>
+            <Link
+              to="/settings/import/googlespreadsheet"
+              style={{ textDecoration: "none" }}
+            >
+              <ListItem>
+                <KeyboardArrowRight className="material-icons" />{" "}
+                <div className="row">
+                  <div className="col-xs-6">
+                    {messages.settings_spreadsheet}
+                  </div>
+                </div>
+              </ListItem>
+            </Link>
+          </List>
+        </div>
+      </Paper>
+    </>
+  )
 }
+
+export default ImportSettings
