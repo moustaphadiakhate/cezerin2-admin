67f1072cf29a470b812c1090debd8c7aee151a2a
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 19:56:15 +0700

    Update index.tsx

--- src/modules/settings/email/index.tsx
+++ src/modules/settings/email/index.tsx
@@ -2,11 +2,11 @@ import { connect } from "react-redux"
 import { fetchEmailSettings } from "../actions"
 import Form from "./components/form"
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state: { settings: { emailSettings: string } }) => ({
   emailSettings: state.settings.emailSettings,
 })
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch: Function) => ({
   onLoad: () => {
     dispatch(fetchEmailSettings())
   },
