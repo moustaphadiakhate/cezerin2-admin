5a45e02aff3f6473332fe532f5934655b9020cb3
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-02 19:55:23 +0700

    Update index.tsx

--- src/modules/settings/checkoutFields/index.tsx
+++ src/modules/settings/checkoutFields/index.tsx
@@ -2,16 +2,16 @@ import { connect } from "react-redux"
 import { fetchCheckoutField, updateCheckoutField } from "../actions"
 import Form from "./components/form"
 
-const mapStateToProps = (state, ownProps) => ({
+const mapStateToProps = (state: { settings: { checkoutField: string } }) => ({
   initialValues: state.settings.checkoutField,
 })
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
+const mapDispatchToProps = (dispatch: Function, ownProps: any) => ({
   onLoad: () => {
     const { fieldName } = ownProps.match.params
     dispatch(fetchCheckoutField(fieldName))
   },
-  onSubmit: values => {
+  onSubmit: (values: string) => {
     dispatch(updateCheckoutField(values))
     ownProps.history.push("/settings/checkout")
   },
