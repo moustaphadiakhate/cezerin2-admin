538006d4d0cdb4107df58ff24254d58553652477
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-03 09:12:02 +0700

    Update multiselectList.tsx

--- src/modules/productCategories/components/multiselectList.tsx
+++ src/modules/productCategories/components/multiselectList.tsx
@@ -1,6 +1,5 @@
-import Checkbox from "material-ui/Checkbox"
-import FontIcon from "material-ui/FontIcon"
-import { List, ListItem } from "material-ui/List"
+import { Checkbox, List, ListItem } from "@material-ui/core"
+import { Folder, VisibilityOff } from "@material-ui/icons"
 import React from "react"
 
 const styles = {
@@ -15,44 +14,36 @@ const styles = {
   },
 }
 
-const FolderIcon = <FontIcon className="material-icons">folder</FontIcon>
-const DraftIcon = <FontIcon className="material-icons">visibility_off</FontIcon>
+const FolderIcon = <Folder className="material-icons" />
+const DraftIcon = <VisibilityOff className="material-icons" />
 
-class Item extends React.PureComponent {
-  handleCheck = (event, isInputChecked) => {
-    const { item } = this.props
-    this.props.onCheck(item.id)
+const Item = props => {
+  const handleCheck = (event, isInputChecked) => {
+    const { item } = props
+    props.onCheck(item.id)
   }
 
-  render() {
-    const { item, opened, selectedIds, nestedItems } = this.props
-    const isChecked =
-      selectedIds && selectedIds.length > 0 && selectedIds.includes(item.id)
-    // const style = isChecked ? styles.selectedItem : null;
+  const { item, opened, selectedIds, nestedItems } = props
+  const isChecked =
+    selectedIds && selectedIds.length > 0 && selectedIds.includes(item.id)
+  // const style = isChecked ? styles.selectedItem : null;
 
-    return (
-      <ListItem
-        className="treeItem"
-        initiallyOpen={opened}
-        innerDivStyle={styles.innerItem}
-        primaryText={item.name}
-        nestedItems={nestedItems}
-        leftCheckbox={
-          <Checkbox checked={isChecked} onCheck={this.handleCheck} />
-        }
-        nestedListStyle={styles.nestedListStyle}
-      />
-    )
-  }
+  return (
+    <ListItem
+      className="treeItem"
+      initiallyOpen={opened}
+      innerDivStyle={styles.innerItem}
+      primaryText={item.name}
+      nestedItems={nestedItems}
+      leftCheckbox={<Checkbox checked={isChecked} onCheck={handleCheck} />}
+      nestedListStyle={styles.nestedListStyle}
+    />
+  )
 }
 
-class Categories extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  getItem(selectedIds, allItems, item, opened) {
-    const nestedItems = this.getChildren(selectedIds, allItems, item.id, opened)
+const Categories = props => {
+  function getItem(selectedIds, allItems, item, opened) {
+    const nestedItems = getChildren(selectedIds, allItems, item.id, opened)
     return (
       <Item
         key={item.id}
@@ -60,28 +51,27 @@ class Categories extends React.Component {
         opened={opened}
         selectedIds={selectedIds}
         nestedItems={nestedItems}
-        onCheck={this.props.onCheck}
+        onCheck={props.onCheck}
       />
     )
   }
 
-  getChildren(selectedIds, allItems, id, opened) {
+  function getChildren(selectedIds, allItems, id, opened) {
     if (allItems && id) {
       return allItems
         .filter(item => item.parent_id === id)
-        .map(item => this.getItem(selectedIds, allItems, item, opened))
+        .map(item => getItem(selectedIds, allItems, item, opened))
     }
     return []
   }
 
-  render() {
-    const { selectedIds, items, opened = false } = this.props
+  const { selectedIds, items, opened = false } = props
 
-    const rows = items
-      .filter(item => item.parent_id === null)
-      .map(item => this.getItem(selectedIds, items, item, opened))
+  const rows = items
+    .filter(item => item.parent_id === null)
+    .map(item => getItem(selectedIds, items, item, opened))
 
-    return <List>{rows}</List>
-  }
+  return <List>{rows}</List>
 }
+
 export default Categories
