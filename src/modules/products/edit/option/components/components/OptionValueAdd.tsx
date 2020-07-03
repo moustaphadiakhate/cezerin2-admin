49f50ab7982e12f57902cc055f059d4af0df169f
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-04 00:12:44 +0700

    Update OptionValueAdd.tsx

--- src/modules/products/edit/option/components/components/OptionValueAdd.tsx
+++ src/modules/products/edit/option/components/components/OptionValueAdd.tsx
@@ -1,94 +1,45 @@
 import FontIcon from "material-ui/FontIcon"
 import IconButton from "material-ui/IconButton"
-import Paper from "material-ui/Paper"
-import React from "react"
+import React, { useState } from "react"
 import messages from "../../../../../../lib/text"
 import style from "./../style.css"
 
-class OptionValueAdd extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      value: "",
-    }
-    this.onChange = this.onChange.bind(this)
-    this.onCreate = this.onCreate.bind(this)
-    this.handleKeyPress = this.handleKeyPress.bind(this)
-  }
+const OptionValueAdd = props => {
+  const [value, setValues] = useState("")
 
-  onChange = e => {
-    this.setState({ value: e.target.value })
-  }
-
-  onCreate = () => {
-    if (this.state.value !== "") {
-      this.props.onCreate(this.state.value)
-      this.setState({ value: "" })
+  const onCreate = () => {
+    if (value !== "") {
+      props.onCreate(value)
+      setValues("")
     }
   }
 
-  handleKeyPress(e) {
-    if (e.keyCode === 13 || e.which === 13) {
-      this.onCreate()
+  function handleKeyPress(event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      onCreate()
     }
   }
-
-  render() {
-    const { value } = this.state
-
-    return (
-      <div className={style.gridRow}>
-        <div className={style.gridColInput}>
-          <input
-            type="text"
-            className={style.textInput}
-            value={value}
-            placeholder={messages.newOptionValue}
-            onChange={this.onChange}
-            onKeyPress={this.handleKeyPress}
-          />
-        </div>
-        <div className={style.gridColButton}>
-          <IconButton
-            title={messages.add}
-            onClick={this.onCreate}
-            tabIndex={-1}
-          >
-            <FontIcon color="#a1a1a1" className="material-icons">
-              add_circle
-            </FontIcon>
-          </IconButton>
-        </div>
-      </div>
-    )
-  }
-}
-
-const OptionValues = ({
-  optionValues,
-  createOptionValue,
-  updateOptionValue,
-  deleteOptionValue,
-}) => {
-  const valueRows = optionValues.map((value, index) => (
-    <OptionValueEdit
-      key={index}
-      value={value}
-      onChange={updateOptionValue}
-      onDelete={deleteOptionValue}
-    />
-  ))
 
   return (
-    <Paper className="paper-box" zDepth={1}>
-      <div className="blue-title" style={{ padding: "20px 30px" }}>
-        {messages.optionValues}
+    <div className={style.gridRow}>
+      <div className={style.gridColInput}>
+        <input
+          type="text"
+          className={style.textInput}
+          value={value}
+          placeholder={messages.newOptionValue}
+          onChange={event => setValues(event.target.value)}
+          onKeyPress={handleKeyPress}
+        />
       </div>
-      <div className={style.grid}>
-        {valueRows}
-        <OptionValueAdd onCreate={createOptionValue} />
+      <div className={style.gridColButton}>
+        <IconButton title={messages.add} onClick={onCreate} tabIndex={-1}>
+          <FontIcon color="#a1a1a1" className="material-icons">
+            add_circle
+          </FontIcon>
+        </IconButton>
       </div>
-    </Paper>
+    </div>
   )
 }
 
