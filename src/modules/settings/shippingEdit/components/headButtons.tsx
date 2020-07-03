c1e300cfb129b9f6e3aeade78693f0db832fe1b9
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2020-07-03 23:08:49 +0700

    Update headButtons.tsx

--- src/modules/settings/shippingEdit/components/headButtons.tsx
+++ src/modules/settings/shippingEdit/components/headButtons.tsx
@@ -1,59 +1,42 @@
-import FontIcon from "material-ui/FontIcon"
-import IconButton from "material-ui/IconButton"
-import React from "react"
+import { IconButton } from "@material-ui/core"
+import { Delete } from "@material-ui/icons"
+import React, { useState } from "react"
 import messages from "../../../../lib/text"
 import DeleteConfirmation from "../../../../modules/shared/deleteConfirmation"
-class Buttons extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      openDelete: false,
-    }
+const Buttons = props => {
+  const [openDelete, setOpenDelete] = useState(false)
+
+  const deleteGroup = () => {
+    setOpenDelete(false)
+    props.onDelete(props.shippingMethod.id)
   }
 
-  showDelete = () => {
-    this.setState({ openDelete: true })
-  }
+  const { shippingMethod, onDelete } = props
+  const methodName =
+    shippingMethod && shippingMethod.name && shippingMethod.name.length > 0
+      ? shippingMethod.name
+      : "Draft"
 
-  closeDelete = () => {
-    this.setState({ openDelete: false })
-  }
-
-  deleteGroup = () => {
-    this.setState({ openDelete: false })
-    this.props.onDelete(this.props.shippingMethod.id)
-  }
-
-  render() {
-    const { shippingMethod, onDelete } = this.props
-    const methodName =
-      shippingMethod && shippingMethod.name && shippingMethod.name.length > 0
-        ? shippingMethod.name
-        : "Draft"
-
-    return (
-      <span>
-        <IconButton
-          touch
-          tooltipPosition="bottom-left"
-          tooltip={messages.actions_delete}
-          onClick={this.showDelete}
-        >
-          <FontIcon color="#fff" className="material-icons">
-            delete
-          </FontIcon>
-        </IconButton>
-        <DeleteConfirmation
-          open={this.state.openDelete}
-          isSingle
-          itemsCount={1}
-          itemName={methodName}
-          onCancel={this.closeDelete}
-          onDelete={this.deleteGroup}
-        />
-      </span>
-    )
-  }
+  return (
+    <span>
+      <IconButton
+        touch
+        tooltipPosition="bottom-left"
+        tooltip={messages.actions_delete}
+        onClick={() => setOpenDelete(true)}
+      >
+        <Delete color="primary" className="material-icons" />
+      </IconButton>
+      <DeleteConfirmation
+        open={openDelete}
+        isSingle
+        itemsCount={1}
+        itemName={methodName}
+        onCancel={() => setOpenDelete(false)}
+        onDelete={deleteGroup}
+      />
+    </span>
+  )
 }
 
 export default Buttons
